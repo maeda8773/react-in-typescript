07eeceb33d20fdaa8d5420e62f2a3c43f01c0d38
Haruya Maeda <haruya.maeda@prum.jp>
2025-11-30 10:02:05 +0900

    useRef

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Counter: React.FC<{}> = () => {
   const [value, setValue] = useState<number>(0);
@@ -11,11 +11,25 @@ const Counter: React.FC<{}> = () => {
     setValue((prevState) => prevState - 1);
   };
 
+  const renderTimes = useRef<number>(0);
+  useEffect(() => {
+    renderTimes.current = renderTimes.current + 1;
+  });
+  const ref = useRef<HTMLInputElement>(null!); // non-nullアサーション演算子を使ってnullではないことを伝える
+  const focusInput = () => {
+    ref.current.focus();
+    // nullかもしれない場合は,foucs()を呼び出せないのでオプショナルチェイニングを使う
+    // ref.current?.focus();
+  };
+
   return (
     <div>
       <div>value: {value}</div>
       <button onClick={increment}>+1</button>
       <button onClick={decrement}>-1</button>
+      <div>This component was re-rendered {renderTimes.current} times!</div>
+      <input ref={ref} type="text" />
+      <button onClick={focusInput}>Click Me!</button>
     </div>
   );
 };
